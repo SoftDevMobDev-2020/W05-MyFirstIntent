c4f170f1a601f0a1f1527aa34ccb604b975c2b2b
N. A. Ronald <nronald@swin.edu.au>
2020-07-09 11:41:25 +1000

    First commit.

---
+++ app/src/main/java/au/edu/swin/sdmd/w05_myfirstintent/DetailActivity.kt
@@ -0,0 +1,11 @@
+package au.edu.swin.sdmd.w05_myfirstintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
